8b60130ed91266a1561869d91b9c794efef37525
ahjun001 <sudo.ubuntu@disroot.org>
2023-09-01 18:33:05 +0800

    formattting

---
+++ 3.Cultivate/,shellspec_format/my_text.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# ,shellspec_format_spec.sh
+
+eval "$(shellspec - -c) exit 1"
+
+Example 'SIMPLE INPUT FROM /TMP/file'
+When call bash -c "cat /tmp/my_txt.txt | ./,shellspec_format.sh"
+The line 1 of output should equal '#!/usr/bin/env bash'
+The error should eq ''
+The status should be success
+End
+
+Example 'SIMPLE INPUT FROM Data struct'
+Data '#!/usr/bin/env bash'
+When call bash -c './,shellspec_format.sh'
+The line 1 of output should equal '#!/usr/bin/env bash'
+The error should eq ''
+The status should be success
+End
+
+Example 'SIMPLE INPUT FROM Data struct'
+Data
+#|#!/usr/bin/env bash
+#|End'
+End
+When run ./,shellspec_format.sh
+The line 1 of output should equal '#!/usr/bin/env bash'
+The error should eq ''
+The status should be success
+End
