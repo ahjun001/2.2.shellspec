a4d9444490a13a182a128422f23233cef0b949d4
ahjun001 <sudo.ubuntu@disroot.org>
2023-10-24 17:04:11 +0800

    reformatted

--- 3.Cultivate/call_bash/spec/xclip_io2_spec.sh
+++ 3.Cultivate/call_bash/spec/xclip_io2_spec.sh
@@ -6,15 +6,15 @@ eval "$(shellspec - -c) exit 1"
 BeforeEach " sort -r <<<$'b\nc\na' | xclip"
 
 Example "1 XCLIP DOES NOT FAIL IF LINE 1 == 'c' "
-When call bash -c "xclip; pkill xclip"
-The line 1 of output should equal 'c'
-The error should eq ''
-The status should be success
+    When call bash -c "xclip; pkill xclip"
+        The line 1 of output should equal 'c'
+        The error should eq ''
+        The status should be success
 End
 
 Example "2 XCLIP DOES NOT FAIL IF LINE 1 == 'c' "
-When call bash -c xclip && pkill xclip
-The line 1 of output should equal 'c'
-The error should eq ''
-The status should be success
+    When call bash -c xclip && pkill xclip
+        The line 1 of output should equal 'c'
+        The error should eq ''
+        The status should be success
 End
